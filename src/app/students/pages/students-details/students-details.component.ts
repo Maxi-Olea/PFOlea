2af6b5cf4cbc916b13a0453d5245649abbd93fec
Maxi Olea <molea@mobydigital.com>
2022-07-17 21:41:04 -0300

    Heroku configuration fix

--- src/app/students/pages/students-details/students-details.component.ts
+++ src/app/students/pages/students-details/students-details.component.ts
@@ -99,7 +99,6 @@ export class StudentsDetailsComponent implements OnInit, OnDestroy {
       })
       
       );
-      console.log('inscriptions', this.inscriptionsData);
   }
 
   onClickEdit() {
